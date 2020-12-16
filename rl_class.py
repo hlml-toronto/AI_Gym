af4da728c7caac2662b9d7502c07b4422674b5f4
dakirby <39995281+dakirby@users.noreply.github.com>
2020-12-16 16:20:16 -0500

    Implemented HLML_ActorCritic class to allow for custom PyTorch models. Need to add documentation, make it easy for the user to write their own Actor and Critic methods.

--- rl_class.py
+++ rl_class.py
@@ -1,6 +1,47 @@
 from gym.spaces import Box, Discrete
 import gym
 from importlib import import_module
+import torch
+import torch.nn as nn
+
+
+class HLML_ActorCritic(nn.Module):
+    """
+    Must take initialization arguments: observation_space, action_space, **ac_kwargs
+    """
+    def __init__(self, observation_space, action_space, **ac_kwargs):
+        super().__init__()
+        if ac_kwargs['training_alg'] == 'vpg':
+            self.pi = ac_kwargs['model_list'][0]
+            self.v = ac_kwargs['model_list'][1]
+        else:
+            raise NotImplementedError("The training algorithm requested has not been implemented")
+
+        # Check that user models are compatible with algorithm
+        core = import_module("algos.{}.core".format(ac_kwargs['training_alg']))
+        assert issubclass(type(self.pi), core.Actor)  # pi should have Actor methods
+        assert issubclass(type(self.v), core.MLPCritic)  # v should have MLPCritic methods
+
+        if isinstance(action_space, Box):
+            test_act = self.pi._distribution(observation_space.sample())
+            true_act = action_space.sample()
+            if len(test_act) != len(true_act):
+                raise TypeError("\nThe output of pi is of length {} but a vector of length {} was expected\n".format(test_act, true_act))
+        elif isinstance(action_space, Discrete):
+            test_act = self.pi._distribution(torch.from_numpy(observation_space.sample()))
+            if isinstance(test_act, int):
+                raise TypeError("\nThe expected output of pi is a discrete action represented by an integer\n")
+
+    def step(self, obs):
+        with torch.no_grad():
+            pi = self.pi._distribution(obs)
+            a = pi.sample()
+            logp_a = self.pi._log_prob_from_distribution(pi, a)
+            v = self.v(obs)
+        return a.numpy(), v.numpy(), logp_a.numpy()
+
+    def act(self, obs):
+        return self.step(obs)[0]
 
 
 class HLML_RL:
@@ -12,11 +53,17 @@ class HLML_RL:
     Attributes
     ----------
     training_alg : str
-        a keyword string indicating which training algorithm to use
+        A keyword string indicating which training algorithm to use
     model_list : list
-        a list of PyTorch models needed to run the indicated training algorithm
+        (default value is None)
+        A list of PyTorch models needed to run the indicated training algorithm.
+
+        If None is passed then default arguments from core.MLPActorCritic will
+        be used, and default PyTorch models will be generated. If the default
+        architectures are desired but with different hidden layer sizes,
+        keword arguments can be passed through ac_kwargs keword in **kwargs.
     env : str
-        the OpenAI Gym environment name to instantiate and train the RL agent on
+        The OpenAI Gym environment name to instantiate and train the RL agent on
 
     Methods
     -------
@@ -40,35 +87,15 @@ class HLML_RL:
         # Check that the environment has been tested
         if self.env not in ['LunarLander-v2']:
             print("The current environment has not been tested. Run at your own risk!")
-        # Check that the models provided are compatible with the environment
-        test_env = gym.make(self.env)
-        self.observation_space_size = len(test_env.observation_space.sample())
-        if isinstance(test_env.action_space, Discrete):
-            self.action_space_size = 1
-        elif isinstance(test_env.action_space, Box):
-            self.action_space_size = len(test_env.action_space.sample())
-
-        if self.training_alg == 'vpg':
-            pass
-            # TO DO: check that models provided output correct size vectors
-            # Check that V input matches action_space_size and observation_space_size
-            # Check that pi input matches action_space_size
 
     def train(self, *args, **kwargs):
         """
         Run the training algorithm to optimize model parameters for the
         environment provided.
         """
-        mod = import_module("algos.{}.{}".format(self.training_alg, self.training_alg))
-        method = getattr(mod, self.training_alg)
-        core = import_module("algos.{}.core".format(self.training_alg))
-        actorCritic = getattr(core, "MLPActorCritic")
-
-        from utils.mpi_tools import mpi_fork
-
+        # Define default parameters for each training algorithm
         default_kwargs = {'vpg': {'env': lambda: gym.make(self.env),
-                                  'hid': 64,
-                                  'l': 2,
+                                  'ac_kwargs': dict(hidden_sizes=[64]*2),
                                   'gamma': 0.99,
                                   'seed': 0,
                                   'cpu': 1,
@@ -76,16 +103,27 @@ class HLML_RL:
                                   'epochs': 50,
                                   'exp_name': 'vpg'}
                           }
-        IO = default_kwargs[self.training_alg]
-        IO.update(kwargs)
+        IO = default_kwargs[self.training_alg]  # Select appropriate kwargs
+        IO.update(kwargs)  # update with user input
+
+        # Dynamically import source code
+        mod = import_module("algos.{}.{}".format(self.training_alg, self.training_alg))
+        method = getattr(mod, self.training_alg)
+        if self.model_list is None:
+            core = import_module("algos.{}.core".format(self.training_alg))
+            actorCritic = getattr(core, "MLPActorCritic")
+        else:
+            actorCritic = HLML_ActorCritic
+            IO['ac_kwargs'] = {'model_list': self.model_list, 'training_alg': self.training_alg}
+
+        from utils.mpi_tools import mpi_fork
 
         mpi_fork(IO['cpu'])  # run parallel code with mpi
 
         from utils.run_utils import setup_logger_kwargs
         logger_kwargs = setup_logger_kwargs(IO['exp_name'], IO['seed'])
 
-        method(IO['env'], actor_critic=actorCritic,
-               ac_kwargs=dict(hidden_sizes=[IO['hid']]*IO['l']),
+        method(IO['env'], actor_critic=actorCritic, ac_kwargs=IO['ac_kwargs'],
                gamma=IO['gamma'], seed=IO['seed'], steps_per_epoch=IO['steps'],
                epochs=IO['epochs'], logger_kwargs=logger_kwargs)
 
--- run.py
+++ run.py
@@ -1,9 +1,21 @@
 from rl_class import HLML_RL
+from algos.vpg.core import MLPCategoricalActor, MLPCritic
 import gym
+import torch.nn as nn
+
 
 if __name__ == '__main__':
     input = {'training_alg': 'vpg',
              'model_list': None,
              'env': 'LunarLander-v2'}
+
+    test_env = gym.make(input['env'])
+    obs_dim = test_env.observation_space.shape[0]
+    act_dim = test_env.action_space.n
+    pi = MLPCategoricalActor(obs_dim, act_dim, (64, 64), nn.Tanh)
+    v = MLPCritic(obs_dim, (32, 32),  nn.Tanh)
+
+    input['model_list'] = [pi, v]
+
     training_setup = HLML_RL(**input)
     training_setup.train()
