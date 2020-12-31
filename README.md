b3ad31eebb4e471d5a57d6bb3d80200333e1b890
dakirby <39995281+dakirby@users.noreply.github.com>
2020-12-31 09:48:02 -0500

    Removed custom ActorCritic from run.py and placed in custom_ActorCritic.py, since this code is algorithm-specific

--- README.md
+++ README.md
@@ -9,27 +9,19 @@ in the context of a time-limited hackathon.
 
 The `HLML_RL` class is defined in `rl_class.py` and is the main object used for
 writing quick scripts such as `run.py` with custom PyTorch models. To build a
-custom model, the user must write their own Actor and Critic classes, following
-the class templates defined in algos.\<training algorithm\>.core .
-
-To write a custom Actor (i.e. a model for the policy distribution pi) the
-user should implement a new class which inherits from the Actor class in `core`
-and has the methods *_distribution*, *_log_prob_from_distribution*, and
-*forward*.
-
-To write a custom Critic (i.e. for Vanilla Policy Gradient this is the
-on-policy value function) the user should implement a new class which has a
-*forward* method that returns the predicted value (as computed by V) for each
-item in the input batch.
+custom model, the user must write their own ActorCritic in
+custom_ActorCritic.py, following the documentation provided by the
+`HLML_RL.ac_help()` method.
 
 **Training a model:**
-- Modify `input` in `run.py`, which is used as `HLML_RL.train(**input)`
+- Modify `user_input` in `run.py`, which is used to initialize an instance as `HLML_RL(**user_input)`
+- Modify  `train_input` in `run.py`, which is used to set hyperparameters for training in `HLML_RL.train(**train_input)`
 - Run `run.py`
 
 **Hardware Case 1: CPU only, multiple threads**
 - Modify the default parameters for the training algorithm being used
-- Specifically, need to override `cpu: n` in `default_kwargs` defined in `HLML_RL.train`. 
-- Only some algorithms (e.g. VPG) support MPI for multi-processing (use `n=1` otherwise).
+- Specifically, need to override `ncpu: 1` in `user_input` defined in `run.py`.
+- Only some algorithms (e.g. VPG) support MPI for multi-processing (use `ncpu=1` otherwise).
 
 **Hardware Case 1: GPU**
 - TODO
--- run.py
+++ run.py
@@ -7,83 +7,41 @@ from torch.distributions.categorical import Categorical
 import algos.vpg.core as core
 from presets import outdir_from_preset
 from rl_class import HLML_RL, get_IO_dim
+from custom_ActorCritic import customActorCritic
 
 
-# Environments to choose from:  https://github.com/openai/gym/wiki/Table-of-environments (box = continuous)
-#  - 'LunarLander-v2'            (discrete action space)
-#  - 'MountainCarContinuous-v0'  (continuous action space)
-#  - 'BipedalWalker-v3'          (continuous action space)       - Hard
+def main(user_input, train_input):
+    # specify output directory
+    default_out = outdir_from_preset(user_input['training_alg'], user_input['training_alg_variant'])
+    default_out += '_' + user_input['env_str']  # include env in out dir
+    if user_input['run_name'] is not None:
+        default_out = default_out + '_' + user_input['run_name']
+    user_input['exp_name'] = default_out
 
+    # build custom actor critic nn modules
+    if user_input['training_alg_variant'] == 'HLML':
+        user_input['actorCritic'] = customActorCritic
+    else:
+        user_input['actorCritic'] = None
 
-# TODO determine if these are VPG specific; move if so
-class myActor(core.Actor):
-    """
-    Custom user policy model
-    """
-    def __init__(self, obs_dim, act_dim, hidden_sizes, activation):
-        super().__init__()
-        sizes = [obs_dim] + list(hidden_sizes) + [act_dim]
-        output_activation = nn.Identity
-        layers = []
-        for j in range(len(sizes)-1):
-            act = activation if j < len(sizes)-2 else output_activation
-            layers += [nn.Linear(sizes[j], sizes[j+1]), act()]
-        self.logits_net = nn.Sequential(*layers)
+    # train the model
+    training_setup = HLML_RL(**user_input)
+    if user_input['training_alg_variant'] == 'HLML':
+        print("The user's custom ActorCritic class should adhere to the following documentation\n\n\n")
+        training_setup.ac_help()  # print the documentation for a custom ActorCritic
+    training_setup.train(**train_input)
 
-    def _distribution(self, obs):
-        logits = self.logits_net(obs)
-        return Categorical(logits=logits)
-
-    def _log_prob_from_distribution(self, pi, act):
-        return pi.log_prob(act)
-
-
-# TODO determine if these are VPG specific; move if so
-class myCritic(nn.Module):
-    """
-    Custom user on-policy value function model
-    """
-    def __init__(self, obs_dim, hidden_sizes, activation):
-        super().__init__()
-        sizes = [obs_dim] + list(hidden_sizes) + [1]
-        output_activation = nn.Identity
-        layers = []
-        for j in range(len(sizes)-1):
-            act = activation if j < len(sizes)-2 else output_activation
-            layers += [nn.Linear(sizes[j], sizes[j+1]), act()]
-        self.v_net = nn.Sequential(*layers)
-
-    def forward(self, obs):
-        return torch.squeeze(self.v_net(obs), -1)  # Critical to ensure v has right shape.
-
-
-class myActorCritic(nn.Module):
-    """Sample code for a custom ActorCritic class to be used with the VPG
-    training algorithm.
-    """
-    def __init__(self, obs_env, act_env, hidden_sizes=(64, 64),
-                 activation=nn.Tanh):
-        super().__init__()
-        obs_dim, act_dim = get_IO_dim((obs_env, act_env))
-        # policy builder depends on action space
-        self.pi = myActor(obs_dim, act_dim, hidden_sizes, activation)
-
-        # build value function
-        self.v = myCritic(obs_dim, hidden_sizes, activation)
-
-    def step(self, obs):
-        with torch.no_grad():
-            pi = self.pi._distribution(obs)
-            a = pi.sample()
-            logp_a = self.pi._log_prob_from_distribution(pi, a)
-            v = self.v(obs)
-        return a.numpy(), v.numpy(), logp_a.numpy()
-
-    def act(self, obs):
-        return self.step(obs)[0]
+    # render the trained model
+    training_setup.render(save=True, show=False)
 
 
 if __name__ == '__main__':
+    # Environments to choose from:  https://github.com/openai/gym/wiki/Table-of-environments (box = continuous)
+    #  - 'LunarLander-v2'            (discrete action space)
+    #  - 'MountainCarContinuous-v0'  (continuous action space)
+    #  - 'BipedalWalker-v3'          (continuous action space)       - Hard
+
+
     # general user choices
     #  - training_alg:            (str) choose a training algorithm (e.g. 'vpg' or 'ddpg')
     #  - training_alg_variant:    (str) choose algorithm variant: 'HLML' or 'spinningup'
@@ -101,26 +59,5 @@ if __name__ == '__main__':
     train_input = {'epochs': 1,
                    'steps_per_epoch': 1000}
 
-    # specify output directory
-    default_out = outdir_from_preset(user_input['training_alg'], user_input['training_alg_variant'])
-    default_out += '_' + user_input['env_str']  # include env in out dir
-    if user_input['run_name'] is not None:
-        user_out = default_out + '_' + user_input['run_name']
-    user_input['exp_name'] = default_out
-
-    # build custom actor critic nn modules
-    if user_input['training_alg_variant'] == 'HLML':
-        # TODO maybe move this out, see comments in presets.py
-        user_input['actorCritic'] = myActorCritic
-    else:
-        user_input['actorCritic'] = None
-
-    # train the model
-    training_setup = HLML_RL(**user_input)
-    if user_input['training_alg_variant'] == 'HLML':
-        print("The user's custom ActorCritic class should adhere to the following documentation\n\n\n")
-        training_setup.ac_help()  # print the documentation for a custom ActorCritic
-    training_setup.train(**train_input)
-
-    # render the trained model
-    training_setup.render(save=True, show=False)
+    # run training, visualize trainined model
+    main(user_input, train_input)
