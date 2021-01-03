dac847d1b04bb65312df3283e1bbb1cdf017a354
dakirby <39995281+dakirby@users.noreply.github.com>
2021-01-03 08:52:02 -0500

    Added script to benchmark algorithm run times

---
+++ benchmark.py
@@ -0,0 +1,55 @@
+from presets import outdir_from_preset, PRESETS
+from rl_class import HLML_RL
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+
+
+def benchmark(algorithm):
+    """ Benchmark the specified algorithm against LunardLander-v2 with one
+    thread. A fully trained model should score between 100 and 140 on a single
+    run of the game.
+    """
+    # All algorithms are benchmarked against LunarLander-v2 with one thread
+    user_input = {'training_alg': algorithm,
+                  'env_str': 'LunarLander-v2',
+                  'ncpu': 1,
+                  'run_name': 'benchmark',
+                  'use_custom': False,
+                  'actorCritic': None}
+
+    train_input = PRESETS[algorithm]
+    train_input['epochs'] = 5000
+
+    # specify output directory
+    default_out = outdir_from_preset(algorithm, False, 'LunarLander-v2') + '_' + 'benchmark'
+    user_input['exp_name'] = default_out
+
+    # train the model
+    bench = HLML_RL(**user_input)
+    bench.train(**train_input)
+
+    # benchmark the trained model
+    log_out = os.path.join('experiments', default_out, default_out + '_s' + str(bench.seed))
+    fname = log_out + os.sep + 'progress.txt'
+    df = pd.read_csv(fname, delim_whitespace=True)
+
+    fig, axes = plt.subplots(nrows=1, ncols=1)
+    x = df['Time']
+    y = df['AverageEpRet']
+    yup = df['AverageEpRet'] + df['StdEpRet']
+    ydown = df['AverageEpRet'] - df['StdEpRet']
+    plt.plot(x, y, 'k--')
+    plt.fill_between(x, ydown, yup, alpha=.3)
+    plt.title(user_input['training_alg'] + ':  ' + user_input['env_str'])
+    plt.xlabel('Wall time')
+    plt.ylabel('Average Epoch Reward')
+    plt.savefig(log_out + os.sep + 'benchmark.png')
+
+
+if __name__ == '__main__':
+    # This is the only line that the user should change:
+    algorithm = 'vpg'
+
+    # run training, evaluate trainined model
+    benchmark(algorithm)
